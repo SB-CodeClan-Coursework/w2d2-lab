001a1e1e4631c23bf0ec0be24934e6f7ba64b76e
Stephen Ramsay <rabrandom@gmail.com>
2019-11-26 16:05:12 +0000

    started extenstions

---
+++ bus_stop.rb
@@ -0,0 +1,29 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../bus')
+require_relative('../person')
+require_relative('../bus_stop')
+
+class BusStopTest < MiniTest::Test
+
+  def setup()
+    @bus_stop = BusStop.new("Princes Street", ["Egg McGee", "Bob John", "Bob John Jr."])
+  end
+
+  def test_get_bus_stop_name
+    assert_equal("Princes Street", @bus_stop.name)
+  end
+
+  def test_
+
+
+
+
+
+
+
+
+
+end
